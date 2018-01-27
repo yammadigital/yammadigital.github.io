bba0f39a3f985cd3b14a628cdc4108c85549b83c
David <david@Davids-MacBook-Pro.local>
2018-01-27 00:06:18 +0000

    buttons and scroll

--- js/main.js
+++ js/main.js
@@ -24,6 +24,15 @@
 		}, 600);
 	});
 
+	$(".learnmore").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+		scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
+
 	$('#back-to-top').on('click', function(){
 		$('body,html').animate({
 			scrollTop: 0
